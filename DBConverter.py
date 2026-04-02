67e41d8d40cf60bb6599c5bad10e57da27604008
MashedUpPeanuts <MashedUpPeanuts@gmail.com>
2026-04-02 22:51:48 +1300

    Add files via upload

--- DBConverter.py
+++ DBConverter.py
@@ -1,242 +1,285 @@
-#V1.02.00
+#####################
+# VERSION 1.02.01 #
+#####################
 
-#Importing libraries
+###################
+# LIBRARY IMPORTS #
+###################
+
 import os
 import sqlite3
 import csv
-
 from tkinter import *
 from tkinter import filedialog
-from tkinter.font import Font
 
+########################
+# VARIABLE DEFINITIONS #
+########################
+
+extensionsAllowable = [".DB", ".db", ".db3", ".sqlite", ".sqlite3"] #List of common SQLite3 file extensions
+filePath = FALSE
+outputPath = FALSE
+checkboxVars = []
+
+########################
+# FUNCTION DEFINITIONS #
+########################
+
+#WRITES SELECTED DATABASE TABLE TO A CSV FILE
 def subConvertDB(fullpath, dbFPath, tableName):
+
     con = sqlite3.connect(dbFPath)
     cur = con.cursor()
 
     cols = cur.execute("PRAGMA table_info('%s')" % tableName).fetchall()
 
-    f = open(fullpath, 'w', newline='', encoding='utf-8')
-    writer = csv.writer(f, dialect=csv.excel, quoting=csv.QUOTE_ALL)
-    field_name_row = []
+    fileOpen = open(fullpath, 'w', newline='', encoding='utf-8')
+    writer = csv.writer(fileOpen, dialect=csv.excel, quoting=csv.QUOTE_ALL)
+    fieldNameRow = []
 
+    #DETERMINING FIELD TITLES
     for col in cols:
         col_name = col[1]
-        field_name_row.append(col_name)
-    writer.writerow(field_name_row)  # write field labels in the first row
+        fieldNameRow.append(col_name)
+    writer.writerow(fieldNameRow)
 
-    # Selecting the data from the support tab
+    #FETCHING DATA FROM DB TABLE
     cur.execute("SELECT * FROM " + tableName + ";")
     rows = cur.fetchall()
 
+    #WRITING TO CSV
     for row in rows:
         writer.writerow(row)  # write data row
-    f.closed
+    fileOpen.closed
 
-#Manages setting output file locations and running subConvertDB for each of the required output tables
+#CHECKS WHICH TABLES ARE SELECTED AND LOOPS TO RUN CSV WRITE FUNCTION
 def ConvertDB(dbFile, savePath):
 
+    #CHECKS STATE OF TABLE CHECKLIST CHECKBOXES
     readStates()
 
+    #QUERIES SQLITE3 DATABASE
     con = sqlite3.connect(filePath)
     cur = con.cursor()
-
     cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
     tables = cur.fetchall()
-    table_names = [table[0] for table in tables]
+    tableNames = [table[0] for table in tables]
 
+    #LOOPS subConvertDB FOR EACH SELECTED TABLE
     count = 0
-
     for table in tables:
-        tableName = table_names[count]
+        tableName = tableNames[count]
         path_f = os.path.join(savePath, tableName + ".csv")
-
         if tickStates[count] == True:
             subConvertDB(path_f, dbFile, tableName)
         count += 1
 
-#Opens a dialogue to select an SQLite3 Database file from Windows explorer
+#OPENS FILE EXPLORER DIALOGUE TO SELECT AN SQLITE3 DATABASE
 def openFile():
-    global filePath  # Setting filePath as global variable for use elsewhere
+
+    #OPENING FILE DIALOG
+    global filePath
     filePath = filedialog.askopenfilename(title="Open an SQLite3 Database file",
                                           filetypes=(("SQLite3 Database File", tuple(extensionsAllowable)), ("all files", "*.*")))
-    my_text.configure(state="normal")
-    my_text.delete(1.0, END)  # Clearing existing input
+    #CLEARING EXISTING FILE SELECTION
+    mainTextInput.configure(state="normal")
+    mainTextInput.delete(1.0, END)
 
+    #IF EXTENSION VALID QUERY TABLE AND CREATE CHECKLIST OF DATABASE TABLES. IF INVALID CLEAR CHECKLIST.
     if filePath.endswith(tuple(extensionsAllowable)):
-        my_text.insert(END, filePath)   #Adding new input to text box
+
+        mainTextInput.insert(END, filePath)
 
         con = sqlite3.connect(filePath)
         cur = con.cursor()
 
+        #QUERYING DATABASE
         cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
         tables = cur.fetchall()
-        global table_names
-        table_names=[table[0] for table in tables]
+        global tableNames
+        tableNames=[table[0] for table in tables]
 
-        global checkbox_vars
-        checkbox_vars = []
+        #CLEARING EXISTING CHECKBOXES AND VARIABLES
+        global checkboxVars
+        checkboxVars = []
         rowPos = 2
-
-        for widget in fTable.winfo_children():
+        for widget in checkFrame.winfo_children():
             if isinstance(widget, Checkbutton):
                 widget.destroy()
 
+        #GENERATING CHECKLIST AND VARIABLES
         for table in tables:
             var = BooleanVar()
-            checkbox_vars.append(var)
-            Checkbutton(fTable, text=table, variable = var).grid(row=rowPos, column=1, sticky="W")
+            checkboxVars.append(var)
+            Checkbutton(checkFrame, text=table, variable = var).grid(row=rowPos, column=1, sticky="W")
             rowPos += 1
         updateScrollRegion()
-        selectButton.configure(state="normal")
-    else:       #Clears checklist if an existing database is replaced with an invalid file or no file
-       checkbox_vars=[]
-       for widget in fTable.winfo_children():
+        checkButtonSelect.configure(state="normal")
+    else:
+       checkboxVars=[]
+       for widget in checkFrame.winfo_children():
            if isinstance(widget, Checkbutton):
                widget.destroy()
-       selectButton.configure(text="Select All", state="disabled")
+       checkButtonSelect.configure(text="Select All", state="disabled")
 
+    mainTextInput.configure(state="disabled")
 
-    my_text.configure(state="disabled")
-
-#Opens a dialogue to select a directory from Windows explorer
+#OUTPUT DIRECTORY SELECTION DIALOG.
 def saveLocation():
-    global outputPath  # Setting outputPath as global variable for use elsewhere
-    outputPath = filedialog.askdirectory(mustexist=TRUE)    #Opening dialogue to select file directory
 
-    my_output.configure(state="normal")
-    my_output.delete(1.0, END)  # Clearing existing input
+    global outputPath
+    outputPath = filedialog.askdirectory(mustexist=TRUE)
 
+    #CLEARING ANY EXISTING DIRECTORY SELECTION
+    mainTextOutput.configure(state="normal")
+    mainTextOutput.delete(1.0, END)
+
+    #PRINTING SELECTED DIRECTORY IN TEXT BOX
     if outputPath:
-        my_output.insert(END, outputPath)   #Adding new input to text box
+        mainTextOutput.insert(END, outputPath)
 
-    my_output.configure(state="disabled")
+    mainTextOutput.configure(state="disabled")
 
-#If the selected DB file and directory are suitable, runs the conversion function
-def ConversionWindow():
-    errorOutput.configure(text="")
+#DETERMINES IF THE SELECTED FILE IS SUITABLE, RUNS CONVERSION, OUTPUTS COMPLETION OR ERROR MESSAGE
+def conversionWindow():
+    mainLabelError.configure(text="")
 
     if outputPath and (filePath.endswith(tuple(extensionsAllowable))):
         ConvertDB(filePath, outputPath)
-        errorOutput.configure(text="The selected SQLite3 database table(s) have been successfully converted into CSV file(s)!",
+        mainLabelError.configure(text="The selected SQLite3 database table(s) have been successfully converted into CSV file(s)!",
                                 fg='Dark Green')
-        cancelButton.configure(text="Exit")
+        mainButtonCancel.configure(text="Exit")
     else:
-        #Outputs error if either the selected file or directory are unsuitable
-        errorOutput.configure(text = "Please ensure a suitable file and directory have been selected.", fg='Red')
+        mainLabelError.configure(text = "Please ensure a suitable file and directory have been selected.", fg='Red')
 
-#Immediately exits thr program
+#IMMEDIATELY EXITS THE PROGRAM
 def cancelProcess():
     mainWindow.destroy()
 
-#Reads the states of the tickboxes in the checkWindow
+#READS THE STATES OF THE TABLE CHECKLIST BUTTONS WRITES TO GLOBAL VARIABLE
 def readStates():
     global tickStates
     tickStates = []
-    tickStates = [var.get() for var in checkbox_vars]
+    tickStates = [var.get() for var in checkboxVars]
 
+#SETS ALL TABLE CHECKLIST BUTTONS TO TRUE, TOGGLES SELECT ALL TO UNSELECT ALL
 def selectAll():
-    if checkbox_vars:
-        [var.set(True) for var in checkbox_vars]
-        selectButton.configure(text="Unselect All", command=unselectAll)
+    if checkboxVars:
+        [var.set(True) for var in checkboxVars]
+        checkButtonSelect.configure(text="Unselect All", command=unselectAll)
 
+#SETS ALL TABLE CHECKLIST BUTTONS TO FALSE. TOGGLES UNSELECT ALL TO SELECT ALL
 def unselectAll():
-    if checkbox_vars:
-        [var.set(False) for var in checkbox_vars]
-        selectButton.configure(text="Select All", command = selectAll)
+    if checkboxVars:
+        [var.set(False) for var in checkboxVars]
+        checkButtonSelect.configure(text="Select All", command = selectAll)
 
-extensionsAllowable = [".DB", ".db", ".db3", ".sqlite", ".sqlite3"] #List of common SQLite3 file extensions
+#UPDATES SCROLLABLE REGION IN checkWindow
+def updateScrollRegion():
+    checkContainer.update_idletasks()
+    checkContainer.config(scrollregion=checkFrame.bbox())
 
-#Setting global variables to false in case of immediate exit of file selection
-filePath = FALSE
-outputPath = FALSE
-checkbox_vars = []
+#CREATES A SCROLLABLE FRAME FOR checkWindow
+def createScrollableContainer():
+    checkContainer.config(yscrollcommand=checkScrollbar.set, highlightthickness=0)
+    checkScrollbar.config(orient=VERTICAL, command=checkContainer.yview)
 
-mainWindow = Tk()   #Opening window for file selection
+    checkScrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+    checkContainer.pack(fill=BOTH, side=LEFT, expand=TRUE)
+    checkContainer.create_window(0, 0, window=checkFrame, anchor=NW)
 
-windowWidth = 1200
-windowHeight = 300
+
+##################
+# GUI GEOMETRIES #
+##################
+
+#DISPLAY DIMENSIONS
+root = Tk()
+screenWidth = root.winfo_screenwidth()
+screenHeight = root.winfo_screenheight()
+root.destroy()
+
+#WINDOW GEOMETRIES
+mainWidth = 1200
+mainHeight = 300
 checkWidth = 300
-checkHeight = windowHeight
-screen_width = mainWindow.winfo_screenwidth()
-screen_height = mainWindow.winfo_screenheight()
-windowCornerH = (screen_width - windowWidth - checkWidth)/2
-windowCornerV = (screen_height - windowHeight)/2
-checkCornerH = windowCornerH + windowWidth
-checkCornerV = windowCornerV
+checkHeight = mainHeight
 
-#Basic window geometry
-mainWindow.geometry('%dx%d+%d+%d' % (windowWidth, windowHeight, windowCornerH, windowCornerV))
+#WINDOW POSITIONING
+mainCornerX = (screenWidth - mainWidth - checkWidth) / 2
+mainCornerY = (screenHeight - mainHeight) / 2
+checkCornerX = mainCornerX + mainWidth
+checkCornerY = mainCornerY
+
+
+#############
+# MAIN MENU #
+#############
+
+#INITIALIZING AND CONFIGURING
+mainWindow = Tk()
+mainWindow.geometry('%dx%d+%d+%d' % (mainWidth, mainHeight, mainCornerX, mainCornerY))
 mainWindow.resizable(False, False)
 mainWindow.title("SQLite3 Database Conversion Tool")
 
-#Labels and buttons for the input file selection
-winLabelIn = Label(mainWindow, text="Press the button below to select an SQLite3 database file")
-my_text = Text(mainWindow, width=135, height=1)
-my_text.configure(state="disabled")
-buttonIn = Button(text="Open Database",command=openFile)
+#DATABASE SELECTION WIDGETS
+mainLabelInput = Label(mainWindow, text="Press the button below to select an SQLite3 database file")
+mainTextInput = Text(mainWindow, width=135, height=1)
+mainTextInput.configure(state="disabled")
+mainButtonInput = Button(text="Open Database", command=openFile)
 
-#Labels and buttons for the output directory selection
-winLabelOut = Label(mainWindow, text="Press the button below to select a directory to save the *.csv outputs to")
-my_output = Text(mainWindow, width=135, height=1)
-my_output.configure(state="disabled")
-buttonOut = Button(text="Select Directory", command=saveLocation)
+#OUTPUT DIRECTORY SELECTION WIDGETS
+mainLabelOutput = Label(mainWindow, text="Press the button below to select a directory to save the *.csv outputs to")
+mainTextOutput = Text(mainWindow, width=135, height=1)
+mainTextOutput.configure(state="disabled")
+mainButtonOutput = Button(text="Select Directory", command=saveLocation)
 
-#Button for confirming selections
-confirmButton = Button(text="Confirm Selections", command=ConversionWindow)
+#CONFIRMATION AND CANCEL WIDGETS
+mainButtonConfirm = Button(text="Confirm Selections", command=conversionWindow)
+mainButtonCancel = Button(text="Cancel", command=cancelProcess, width=10)
+mainLabelError=Label(mainWindow, text='', fg='black')
 
-#Button for immediately exiting the program
-cancelButton = Button(text="Cancel", command=cancelProcess, width=10)
+#PACKING MAIN MENU
+mainLabelInput.pack(pady=(5, 0))
+mainTextInput.pack(pady=(5, 0))
+mainButtonInput.pack(pady=(5, 0))
 
-#Error (and success) text output
-errorOutputString = ''
-colourCode = 'black'
-errorOutput=Label(mainWindow, text=errorOutputString, fg=colourCode)
+mainLabelOutput.pack(pady=(20, 0))
+mainTextOutput.pack(pady=(5, 0))
+mainButtonOutput.pack(pady=(5,0))
 
-#Packing display window
-winLabelIn.pack(pady=(5, 0))
-my_text.pack(pady=(5, 0))
-buttonIn.pack(pady=(5, 0))
-winLabelOut.pack(pady=(20, 0))
-my_output.pack(pady=(5, 0))
-buttonOut.pack(pady=(5,0))
-confirmButton.pack(pady=(15, 0))
-cancelButton.pack(pady=(5, 0))
-errorOutput.pack(pady=(10,0))
+mainButtonConfirm.pack(pady=(15, 0))
+mainButtonCancel.pack(pady=(5, 0))
+mainLabelError.pack(pady=(10,0))
 
 
-#Database table selection window
-tickWindow = Toplevel(mainWindow)
-tickWindow.title("Select tables to export")
-tickWindow.geometry('%dx%d+%d+%d' % (checkWidth, checkHeight, checkCornerH, checkCornerV))
-tickWindow.resizable(False, False)
-tickWindow.protocol("WM_DELETE_WINDOW", cancelProcess) #Closes main window when child window is closed
+########################
+# TABLE SELECTION MENU #
+########################
+
+#INITIALIZING AND CONFIGURING
+checkWindow = Toplevel(mainWindow)
+checkWindow.title("Select tables to export")
+checkWindow.geometry('%dx%d+%d+%d' % (checkWidth, checkHeight, checkCornerX, checkCornerY))
+checkWindow.resizable(False, False)
+checkWindow.protocol("WM_DELETE_WINDOW", cancelProcess) #Closes main window when child window is closed
+
+#WINDOW AND SCROLLBAR CONFIGURATION
+checkContainer = Canvas(checkWindow)
+checkFrame = Frame(checkContainer)
+checkScrollbar = Scrollbar(checkWindow)
+
+#SELECT ALL BUTTON
+checkButtonSelect = Button(checkFrame, text="Select All", command=selectAll)
+checkButtonSelect.configure(width=2, height=1)
+checkButtonSelect.grid(row=1, column=1, ipadx=120, padx=10)
+checkButtonSelect.configure(state="disabled")
 
 
-cTableContainer = Canvas(tickWindow)
-fTable = Frame(cTableContainer)
-sbVerticalScrollBar = Scrollbar(tickWindow)
-
-selectButton = Button(fTable, text="Select All", command=selectAll)
-selectButton.configure(width=2, height=1)
-selectButton.grid(row=1, column=1, ipadx=120, padx=10)
-selectButton.configure(state="disabled")
-
-
-def updateScrollRegion():
-    cTableContainer.update_idletasks()
-    cTableContainer.config(scrollregion=fTable.bbox())
-
-def createScrollableContainer():
-    cTableContainer.config(yscrollcommand=sbVerticalScrollBar.set, highlightthickness=0)
-    sbVerticalScrollBar.config(orient=VERTICAL, command=cTableContainer.yview)
-
-    sbVerticalScrollBar.pack(fill=Y, side=RIGHT, expand=FALSE)
-    cTableContainer.pack(fill=BOTH, side=LEFT, expand=TRUE)
-    cTableContainer.create_window(0, 0, window=fTable, anchor=NW)
-
-
+##############
+# MAIN LOOPS #
+##############
 
 createScrollableContainer()
-
-tickWindow.mainloop()
+checkWindow.mainloop()
 mainWindow.mainloop()
